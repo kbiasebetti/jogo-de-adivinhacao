2cc801d50c47917bafe4768e40b1fd4db7a6e8e3
Kauã Biasebetti <kauamengel17@gmail.com>
2025-03-20 20:09:26 -0300

    Adicionando a estrutura básica, entrada do usuário e começo da lógica do jogo.

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
+        // Versão 01: Estrutura Básica e Entrada do Usuário
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("| Jogo de Adivinhação |");
+            Console.WriteLine("-------------------------------");
+
+            // Lógica do Jogo
+            Console.Write("> Digite um número de chute: ");
+            int numeroChute = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Você digitou o número: {numeroChute}");
+
+            Console.ReadLine();
         }
     }
 }
